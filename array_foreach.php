662028da10f5fc4a7277c3a89d6f829511bb739a
Alexchent <1023615292@qq.com>
2019-01-21 00:25:12 +0800

    array_filter&array_walk&array_map

--- array_foreach.php
+++ array_foreach.php
@@ -6,6 +6,19 @@
  * Time: 2:08 PM
  */
 
+#array_filter
+function rule($var){
+    if($var%2 == 0) return true;
+    return false;
+}
+
+$arr=array('a'=>1,'b'=>2,'c'=>3,'d'=>4,'e'=>6);
+$newarr = array_filter($arr,'rule', ARRAY_FILTER_USE_BOTH);
+var_dump($newarr);
+
+
+
+#array_walk 传引用,第三个参数传给回调函数
 $arr = [1,2,3,4,5,6];
 $a = 2;
 function test(&$v, $key, $a) {
@@ -16,8 +29,7 @@ array_walk($arr, 'test', $a);
 
 
 
-
-
+#array_map 传值
 $brr = [1,2,3,4,5,6];
 $test1 = function ($v) use ($a) {
 //    echo $v.'——'.$a."\n";
