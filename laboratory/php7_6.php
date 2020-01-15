1536e21cf1266530b636588a75a9d30d862978b1
Alexchent <1023615292@qq.com>
2020-01-15 16:17:37 +0800

    Closure::call

---
+++ laboratory/php7_6.php
@@ -0,0 +1,1 @@
+<?php/** * Closure::call() 有着更好的性能，将一个闭包函数动态绑定到一个新的对象实例并调用执行该函数 */class A {    private $x =1;}// PHP 7 之前版本定义闭包函数代码$getXCB = function() {    return $this->x;};// 闭包函数绑定到类 A 上$res = $getXCB->bindTo(new A, 'A');echo $res().PHP_EOL;//结果 1// php7echo $getXCB->call(new A);//结果 1
