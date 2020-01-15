c153e49a9963752d5b8c017c3a638978910a1604
Alexchent <1023615292@qq.com>
2020-01-15 14:05:25 +0800

    null合并运算符

---
+++ laboratory/php7_2.php
@@ -0,0 +1,1 @@
+<?php/** * * Null 合并运算符 * PHP 7 新增加的 NULL 合并运算符（??）是用于执行isset()检测的三元运算的快捷方式。 * NULL 合并运算符会判断变量是否存在且值不为NULL，如果是，它就会返回自身的值，否则返回它的第二个操作数。 * * 以前的写法是 */$a = null;$res = isset($a) ? $a : '测试';$res1 = $a ?? 'test';echo $res.PHP_EOL;echo $res1.PHP_EOL;
